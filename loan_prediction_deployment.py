5a0de08e1d84345b0b6d6557b949e1382f6339cc
anushka0720 <csanushkasalvi@gmail.com>
2026-02-19 13:06:29 +0530

    Update loan_prediction_deployment.py

--- loan_prediction_deployment.py
+++ loan_prediction_deployment.py
@@ -12,8 +12,8 @@ import pandas as pd
 import joblib
 
 # Load trained model and encoders
-model = joblib.load("salary_prediction_model(1).pkl")
-encoder = joblib.load("label_encoder(1).pkl")
+model = joblib.load("salary_prediction_model (1).pkl")
+encoder = joblib.load("label_encoder (1).pkl")
 
 st.title("Loan Prediction App")
 
